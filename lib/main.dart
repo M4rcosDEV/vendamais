efda74fb059b5089dfb908b3860d472b49557025
M4rcosDEV <marcosifba01@gmail.com>
2024-09-29 13:11:02 -0300

    Alterações nas opções de analise do flutter

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
